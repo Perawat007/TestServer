691cd2ac3f5ac5bf89df2d76b5f99629cce8d7fb
Perawat007 <96761242+Perawat007@users.noreply.github.com>
2023-04-08 21:51:05 +0700

    editrequiew

--- main.js
+++ main.js
@@ -16,10 +16,11 @@ app.use(bodyParser.json());
 app.listen(5000, () => console.log(`Listening on port... ${5000}`));
 
 const connection = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'db_gggame',
+    port:3306
 })
 
 //http://localhost:5000/list_users
@@ -35,51 +36,46 @@ app.get('/list_users',(require,response)=>{
     });
 });
 
-app.get('/list_users/:id',(req,res)=>{
-    let qrId = req.params.id;
-    //let qr = `SELECT * FROM admindata where id = ${qrId}`;
-    let sql = `SELECT id, user_code, name FROM member WHERE id = ${qrId} ORDER BY user_code ASC`;
-    connection.query(sql,(err,results) =>{
-        if (err){
-            console.log(err)
-        }
-        if (results.length>0){
-            res.send({
-                message: 'Get Data By Id',
-                data:results
-            });
-        }else{
-            res.send({
-                message:"Data not found dear"
-            })
-        }
-    });
+app.get('/list_users/:id',(require,response)=>{
+    let user_id = require.params.id;
+    console.log(user_id);
+    let sql = `SELECT id, user_code, name, balance FROM member WHERE id='${user_id}' AND status='Y' ORDER BY user_code ASC`;
+    connection.query(sql,(error,results)=>{
+        if(error){ console.log(error) }
+        response.send({
+            //message:"Data Update Success", 
+            data: results
+        });
+        return response;
+    })
 });
 
 app.get('/user_play/user/:user_id',(require,response)=>{
-    let user_id = require.params.user_id;
+    let user_id = require.params.id;
     let sql = `SELECT member.id AS member_id, member.user_code AS user_code, member.name AS name, member.balance AS balance, 
     user_play.bet AS bet, user_play.win AS win, user_play.tiles AS tiles, winline AS winline FROM user_play, member 
     WHERE user_play.member_id=member.id AND member.id='${user_id}' AND member.status='Y' ORDER BY member.created_at DESC`;
     connection.query(sql,(error,results)=>{
         if(error){ console.log(error) }
         response.send({
+            message: 'member play',
             data: results
         });
-    });
+        return response;
+    })
 });
 
 app.post('/user_play/add/:user_id',(require,response)=>{
-    let user_id = require.params.user_id;
+    let user_id = require.params.id;
     let bet = require.body.bet;
     let tiles = require.body.tiles;
     let winline = require.body.winline;
     let sql = `INSERT INTO user_play (member_id, bet, win, tiles, winline, created_at) value ('${user_id}','${bet}','${tiles}','${winline}', now())`;
     connection.query(sql,(error,results)=>{
-        if(error){ console.log(error) }
+        if(error){console.log(error)}
         response.send({
             message: "Data created Success",
             data: results
         });
-    });
+    })
 });
