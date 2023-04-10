7c9319e95108b33d50b5ebe30c7a9f94e1dccd44
Perawat007 <96761242+Perawat007@users.noreply.github.com>
2023-04-10 10:56:50 +0700

    new01

--- db_gggame.sql
+++ db_gggame.sql
@@ -1,75 +1,88 @@
-﻿# Host: localhost  (Version 5.5.5-10.4.24-MariaDB)
-# Date: 2023-04-05 09:19:33
-# Generator: MySQL-Front 6.0  (Build 2.20)
+﻿SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-
-#
-# Structure for table "admin"
-#
-
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE `admin` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `datetime_lastlogin` datetime DEFAULT NULL,
-  `get_browser` longtext COLLATE utf8_unicode_ci DEFAULT NULL,
-  `status` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Y',
-  `status_delete` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
+  `get_browser` longtext DEFAULT NULL,
+  `status` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `status_delete` enum('Y','N') NOT NULL DEFAULT 'N',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-#
-# Data for table "admin"
-#
-
-
-#
-# Structure for table "agent"
-#
-
-DROP TABLE IF EXISTS `agent`;
 CREATE TABLE `agent` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `website_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Y',
-  `status_delete` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `website_name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `status_delete` enum('Y','N') NOT NULL DEFAULT 'N',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-#
-# Data for table "agent"
-#
-
-
-#
-# Structure for table "member"
-#
-
-DROP TABLE IF EXISTS `member`;
 CREATE TABLE `member` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `user_code` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Y',
-  `status_delete` enum('Y','N') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'N',
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `agent_id` bigint(20) DEFAULT 0,
+  `user_code` varchar(25) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `balance` float(15,2) DEFAULT 0.00,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` enum('Y','N') NOT NULL DEFAULT 'Y',
+  `status_delete` enum('Y','N') NOT NULL DEFAULT 'N',
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6350 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-#
-# Data for table "member"
-#
+INSERT INTO `member` (`id`, `agent_id`, `user_code`, `name`, `balance`, `username`, `password`, `status`, `status_delete`, `created_at`, `updated_at`) VALUES
+(1, NULL, 'member001', 'member001', 0.00, 'member001', '123', 'Y', 'N', '2023-04-01 01:56:00', '2023-04-01 01:56:00'),
+(2, NULL, 'member002', 'member002', 0.00, 'member001', '123', 'Y', 'N', '2023-04-01 01:56:00', '2023-04-01 01:56:00');
 
-INSERT INTO `member` VALUES (1,'member001','member001','member001','123','Y','N','2023-04-01 08:56:00','2023-04-01 08:56:00'),(2,'member002','member002','member001','123','Y','N','2023-04-01 08:56:00','2023-04-01 08:56:00');
+CREATE TABLE `user_play` (
+  `id` bigint(1) NOT NULL,
+  `member_id` bigint(20) DEFAULT 0,
+  `game_id` bigint(20) DEFAULT 0,
+  `bet` float(16,2) DEFAULT 0.00,
+  `win` float(16,2) DEFAULT 0.00,
+  `tiles` longtext DEFAULT NULL,
+  `winline` int(11) DEFAULT 0,
+  `get_browser` longtext DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `user_play` (`id`, `member_id`, `game_id`, `bet`, `win`, `tiles`, `winline`, `get_browser`, `created_at`) VALUES
+(1, 1, 1, 10.00, 10.00, 'index1,index2', 1, NULL, '2023-04-09 22:44:01'),
+(2, 1, 1, 10.00, 10.00, 'index1,index2', 1, NULL, '2023-04-09 22:44:45'),
+(3, 1, 1, 10.00, 10.00, 'index1,index2', 1, NULL, '2023-04-09 22:45:16');
+
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `agent`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `user_play`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `admin`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `agent`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `member`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE `user_play`
+  MODIFY `id` bigint(1) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
--- main.js
+++ main.js
@@ -6,8 +6,9 @@ const app = express();
 const useragent = require('express-useragent');
 const ejs = require('ejs');
 const mysql = require('mysql2') //npm install mysql2
+const axios = require('axios'); //npm install axios
 var cors = require('cors');
-const { exit } = require('process');
+
 require('dotenv').config()
 app.engine("html", ejs.renderFile);
 app.use(useragent.express());
@@ -48,7 +49,7 @@ app.get('/list_admin/:admin_id',(require,response)=>{
         });
 
         return response;
-    })
+    });
 });
 
 //http://localhost:5000/list_agents
@@ -79,7 +80,7 @@ app.get('/list_agent/:agent_id',(require,response)=>{
         });
 
         return response;
-    })
+    });
 });
 
 //http://localhost:5000/list_users/agent/1
@@ -112,7 +113,7 @@ app.get('/list_users',(require,response)=>{
     });
 });
 
-//http:localhost:5000/list_user/1
+http://localhost:5000/list_user/1
 app.get('/list_user/:user_id',(require,response)=>{
     let user_id = require.params.user_id;
     let sql = `SELECT id, member_code, name, username, balance, status FROM member WHERE id='${user_id}' AND status_delete='N' 
@@ -125,12 +126,12 @@ app.get('/list_user/:user_id',(require,response)=>{
         });
 
         return response;
-    })
+    });
 });
 
-//http:localhost:5000/games
+http://localhost:5000/games
 app.get('/games',(require,response)=>{
-    let sql = `SELECT id, game_name, image, status WHERE status_delete='N' ORDER BY game_name ASC`;
+    let sql = `SELECT id, game_name, image, status FROM game WHERE status_delete='N' ORDER BY game_name ASC`;
     connection.query(sql,(error,results) =>{
         if(error){ console.log(error); }
         response.send({
@@ -142,10 +143,10 @@ app.get('/games',(require,response)=>{
     });
 });
 
-//http:localhost:5000/game/1
+http://localhost:5000/game/1
 app.get('/game/:game_id',(require,response)=>{
     let game_id = require.params.game_id;
-    let sql = `SELECT id, game_name, image, status WHERE id='${game_id}' AND status_delete='N' ORDER BY game_name ASC`;
+    let sql = `SELECT id, game_name, image, status FROM game WHERE id='${game_id}' AND status_delete='N' ORDER BY game_name ASC`;
     connection.query(sql,(error,results) =>{
         if(error){ console.log(error); }
         response.send({
@@ -157,7 +158,7 @@ app.get('/game/:game_id',(require,response)=>{
     });
 });
 
-//http:localhost:5000/user_play/user/1
+http://localhost:5000/user_play/user/1
 app.get('/user_play/user/:user_id',(require,response)=>{
     let user_id = require.params.user_id;
     let sql = `SELECT member.id AS member_id, member.member_code AS member_code, member.name AS name, member.balance AS balance, 
@@ -171,23 +172,60 @@ app.get('/user_play/user/:user_id',(require,response)=>{
         });
 
         return response;
-    })
+    });
 });
 
-//http:localhost:5000/user_play/add/1
+
 app.post('/user_play/add/:user_id',(require,response)=>{
     let user_id = require.params.user_id;
+    let game_id = require.body.game_id;
     let bet = require.body.bet;
+    let win = require.body.win;
     let tiles = require.body.tiles;
     let winline = require.body.winline;
-    let sql = `INSERT INTO user_play (member_id, bet, win, tiles, winline, created_at) value ('${user_id}','${bet}','${tiles}','${winline}', now())`;
-    connection.query(sql,(error,results)=>{
-        if(error){console.log(error)}
-        response.send({
-            message: "Data created Success",
-            data: results
-        });
+    let sql_check = `SELECT id, member_code, name, username, balance, status FROM member WHERE id='${user_id}' AND status_delete='N' 
+    ORDER BY member_code ASC`;
+    connection.query(sql_check,(error,results)=>{
+        if(results.length > 0){
+            let sql = `INSERT INTO user_play (member_id, game_id, bet, win, tiles, winline, created_at) value ('${user_id}','${game_id}','${bet}','${win}','${tiles}','${winline}', now())`;
+            connection.query(sql,(error,results)=>{
+                if(error){console.log(error)}
+                response.send({
+                    message: "Data created Success",
+                    data: results
+                });
 
-        return response;
+                return response;
+            });
+        }else{
+            response.send({
+                message: "No Data member",
+                data: results
+            });
+
+            return response;
+        } 
+    });
+});
+
+http://localhost:5000/user_play/add/1
+app.get('/user_play/add/:user_id',(require,response)=>{
+    let user_id = require.params.user_id;
+
+    axios.post('http://localhost:5000/user_play/add/'+user_id,{
+        "member_id": user_id,
+        "game_id": 1,
+        "balance": 100.50,
+        "bet": 10,
+        "win" : 10,
+        "tiles":["index1", "index2"],
+        'winline': 1
     })
+    .then(res => {
+        console.log(res.data);
+    })
+    .catch(error =>{
+        console.log(error);
+    });
+
 });
